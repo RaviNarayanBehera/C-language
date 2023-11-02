46a9337a9eacde05db802a0e7b2efceaf972561e
Ravi Narayan Behera <148530184+RaviNarayanBehera@users.noreply.github.com>
2023-11-02 23:17:28 +0530

    Add files via upload

---
+++ Simple Program/Arithmatic.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+
+{
+	int a,b;
+	a=500;
+	b=5;
+	
+	printf("a+b=%d",a+b);//addition;
+	printf("\na-b=%d",a-b);//substraction;
+	printf("\na/b=%d",a/b);//division;
+	printf("\na*b=%d",a*b);//multiplication;
+	printf("\na%%b=%d",a%b);//module;
+}
