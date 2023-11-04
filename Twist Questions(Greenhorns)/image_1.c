ab3913547abbe66363d44a9008efeb01dbe935e0
Ravi Narayan Behera <148530184+RaviNarayanBehera@users.noreply.github.com>
2023-11-04 13:12:31 +0530

    Add files via upload

---
+++ Twist Questions(Greenhorns)/image_1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	printf("Image 1");
+	printf("\n\n\t\t*\t\t*");
+	printf("\n\n\t*\t\t*");
+	printf("\n\n*\t\t*");
+	printf("\n\n\t*\t\t*");
+	printf("\n\n\t\t*\t\t*");
+}
