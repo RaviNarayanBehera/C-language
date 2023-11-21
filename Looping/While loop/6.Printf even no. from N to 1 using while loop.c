4a021be87eb982fe3216743f9d047a3273cc5972
Ravi Narayan Behera <148530184+RaviNarayanBehera@users.noreply.github.com>
2023-11-21 21:57:57 +0530

    Add files via upload

---
+++ Looping/While loop/6.Printf even no. from N to 1 using while loop.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int a=1,even;
+	
+	printf("Enter no.:= ");
+	scanf("%d",&even);
+	
+	while(even>=a)
+	{	
+		if(even%2==0)
+		{
+			printf("%d ",even);	
+		}
+		even--;
+	}
+}
+
