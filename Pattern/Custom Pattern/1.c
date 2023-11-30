eb507c0746981d427cd58b6a4741a692facb8fe7
Ravi Narayan Behera <148530184+RaviNarayanBehera@users.noreply.github.com>
2023-11-30 21:50:35 +0530

    Add files via upload

---
+++ Pattern/Custom Pattern/1.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int i,j,k;
+	
+	for(i=5; i>=1; i--)
+	{
+		for(j=1; j<=i; j++)
+		{
+			printf("%d",j);
+		}
+		printf("\n");
+	}
+	
+	for(i=1; i<=5; i++)
+	{
+		for(j=1; j<=i; j++)
+		{
+			printf("%d",j);
+		}	
+		printf("\n");
+	}
+	
+}
+/*if(j%2==0)
+			{
+				printf("* ");
+			}
+			else
+			{
+				printf(" ");
+			}*/
