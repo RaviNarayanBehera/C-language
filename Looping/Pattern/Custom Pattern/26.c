bc5b838b5730b3c50dbeb41290bc96e71abeaf5b
Ravi Narayan Behera <148530184+RaviNarayanBehera@users.noreply.github.com>
2023-12-01 22:17:19 +0530

    Add files via upload

---
+++ Looping/Pattern/Custom Pattern/26.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int i,j;
+	
+	for(i=1; i<=7; i++)
+	{
+		for(j=1; j<=5; j++)
+		{
+			if((j==1 && i!=7) || (i==7 && j!=1 && j!=5) || (j==5 && i!=7))
+			{
+				printf("*");	
+			}
+			else
+			{
+				printf(" ");
+			}
+		}
+		printf("\n");
+	}
+}
