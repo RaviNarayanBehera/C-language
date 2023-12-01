0420e37bb93b862497d1601ae6bd20d00f89da2b
Ravi Narayan Behera <148530184+RaviNarayanBehera@users.noreply.github.com>
2023-12-01 22:12:37 +0530

    Add files via upload

---
+++ Looping/Pattern/Simple Pattern/18.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int i,j;
+	
+	for(i=1; i<=5; i++)
+	{
+		for(j=1; j<=i; j++)
+		{
+			if(j%2==1)
+			{
+				printf("0 ");
+			}
+			else
+			{
+				printf("1 ");
+			}
+		}
+		printf("\n");
+	}
+}
