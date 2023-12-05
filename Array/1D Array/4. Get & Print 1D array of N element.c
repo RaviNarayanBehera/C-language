3e06b11620029bb7da45e1a2eeb045e857f2487c
Ravi Narayan Behera <148530184+RaviNarayanBehera@users.noreply.github.com>
2023-12-05 22:00:15 +0530

    Add files via upload

---
+++ Array/1D Array/4. Get & Print 1D array of N element.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int n; 
+	
+	printf("Enter array size : ");
+	scanf("%d",&n);
+	
+	int i,a[n];
+	
+	printf("\nEnter array element : \n\n");
+	
+	for(i=0; i<n; i++)
+	{
+		printf("a[%d] : ",i);
+		scanf("%d",&a[i]);
+	}
+	
+	printf("\n");
+	
+	for(i=0; i<n; i++)
+	{
+		printf("%d ",a[i]);
+	}
+	
+	
+	
+}
