ddbe5427bebf74ba2b309835dbd67f1c2f27e70b
Ravi Narayan Behera <148530184+RaviNarayanBehera@users.noreply.github.com>
2023-12-06 21:56:52 +0530

    Add files via upload

---
+++ Array/1D array exercise/1. Merge operation of two 1D arrays & Store it in another array.c
@@ -0,0 +1,67 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int x,y;
+	
+	printf("Enter array A's size : ");
+	scanf("%d",&x);
+	printf("Enter Elements of A :\n");
+// For enter array of A element....		
+	int i,a[x];
+	
+	for(i=0; i<x; i++)
+	{
+		printf("a[%d] : ",i);
+		scanf("%d",&a[i]);
+	}
+	
+	printf("\nArray A's Element : ");
+	
+	for(i=0; i<x; i++)
+	{
+		printf("%d ",a[i]);
+	}
+// For enter array of B element....		
+	printf("\n\nEnter array B's size : ");
+	scanf("%d",&y);
+	printf("Enter Elements of B :\n");
+	
+	int j,b[y];
+	
+	for(j=0; j<y; j++)
+	{
+		printf("b[%d] : ",j);
+		scanf("%d",&b[j]);
+	}
+	
+	printf("\nArray B's Element : ");
+	
+	for(j=0; j<y; j++)
+	{
+		printf("%d ",b[j]);
+	}
+	
+// Now Merging of A & B in new array 'C'.....
+
+	int c[x+y];
+	
+	for(i=0; i<x; i++)
+	{
+		c[i]=a[i];
+	}
+	
+	for(j=0; j<y; j++)
+	{
+		c[x+j]=b[j];
+	}
+	
+	printf("\n\nArray C is : ");
+	for(i=0; i<x+y; i++)
+	{
+		printf("%d ",c[i]);
+	}
+	
+	
+			
+}
