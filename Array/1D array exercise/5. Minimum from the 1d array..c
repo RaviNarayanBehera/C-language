82617f7bb6d0670812878ea6d2dc7fe487f89d0d
Ravi Narayan Behera <148530184+RaviNarayanBehera@users.noreply.github.com>
2023-12-07 21:59:38 +0530

    Add files via upload

--- Array/1D array exercise/5. Minimum from the 1d array..c
+++ Array/1D array exercise/5. Minimum from the 1d array..c
@@ -26,35 +26,3 @@ main()
 	}
 	printf("\nMinimum number : %d",min);
 }
-
-
-
-
-
-
-
-/*
-#include<stdio.h>
-main()
-{
-	int n;
-	printf("enter n:");
-	scanf("%d",&n);
-	int a[n];
-	int i;
-	for(i=0; i<=n; i++)
-	{
-		
-		printf("enter a[%d]",i);
-		scanf("%d",&a[i]);
-	}
-	int min=0;
-	for(i=0; i<=n; i++)
-	{
-	if(a[i<min])
-	{
-	   	min=a[i];
-	}
-   }
-	printf("%d",min);
-}*/
