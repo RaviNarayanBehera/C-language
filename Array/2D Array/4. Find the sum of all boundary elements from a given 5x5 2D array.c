7d00c40a0be8a9165b4b09233b48e30a07769035
Ravi Narayan Behera <148530184+RaviNarayanBehera@users.noreply.github.com>
2023-12-08 20:38:27 +0530

    Add files via upload

---
+++ Array/2D Array/4. Find the sum of all boundary elements from a given 5x5 2D array.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	printf("Enter array's element :\n");
+	int i,j,a[5][5];
+	
+	for(i=0; i<5; i++)
+	{
+		for(j=0; j<5; j++)
+		{
+			printf("a[%d][%d] : ",i,j);
+			scanf("%d",&a[i][j]);
+		}
+		printf("\n");
+	}
+	
+	printf("\n");
+	
+	int sum;
+	
+	for(i=0; i<5; i++)
+	{
+		for(j=0; j<5; j++)
+		{
+			if(i==0 || i==4 || j==0 || j==4)
+			{
+				printf("%d ",a[i][j]);
+				sum=sum+a[i][j];
+			}
+			else
+			{
+				printf("  ");
+			}
+			
+		}
+		printf("\n");
+	}
+	printf("\nSum of Boundary elements of an array : %d",sum);
+	
+	
+}
