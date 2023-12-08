1b9f9144971be31a216713b7be32c03b358d9c7b
Ravi Narayan Behera <148530184+RaviNarayanBehera@users.noreply.github.com>
2023-12-08 20:37:01 +0530

    Add files via upload

---
+++ Array/2D Array/3. Find the sum of diagonal elements from a given 2D array.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int m;
+	
+	printf("Enter size of Row & Column : ");
+	scanf("%d",&m);
+	
+	int a[m][m],i,j;
+	
+	printf("\nEnter array's element : \n");
+	
+	for(i=0; i<m; i++)
+	{
+		for(j=0; j<m; j++)
+		{
+			printf("a[%d][%d] : ",i,j);
+			scanf("%d",&a[i][j]);
+		}
+		printf("\n");
+	}
+
+	int sum;
+	for(i=0; i<m; i++)
+	{
+		sum=sum+a[i][i];	
+	}
+	
+	printf("The sum of diagonal elements of an Array : %d ",sum);
+	
+}
