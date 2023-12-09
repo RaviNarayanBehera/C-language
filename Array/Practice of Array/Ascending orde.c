620dc483d10e772becc6ff0fb9a0649bf3bc2a0f
Ravi Narayan Behera <148530184+RaviNarayanBehera@users.noreply.github.com>
2023-12-09 21:43:22 +0530

    Add files via upload

---
+++ Array/Practice of Array/Ascending orde.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int a;
+	
+	printf("Enter size of array := ");
+	scanf("%d",&a);
+	
+	int m[a],i,j;
+	
+	printf("\nEnter element of array :\n");
+	
+	for(i=0; i<a; i++)
+	{
+		printf("m[%d] : ",i);
+		scanf("%d",&m[i]);
+	}
+	
+	for(i=0; i<a; i++)
+	{
+		for(j=i+1; j<a; j++)
+		{
+			if(m[i]>m[j])
+			{
+				int temp = m[i];
+				m[i] = m[j];
+				m[j] = temp;
+			}
+		}
+	}
+	
+	printf("\nAscending order : ");
+	
+	for(i=0; i<a; i++)
+	{
+		printf("%d ",m[i]);
+	}
+	
+}
