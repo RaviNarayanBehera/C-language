8789f88a07eb23e4f68dc9c748693f1d7b7f6ea7
Ravi Narayan Behera <148530184+RaviNarayanBehera@users.noreply.github.com>
2023-12-09 21:44:45 +0530

    Add files via upload

---
+++ Array/Practice of Array/Decending Order.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int a;
+	
+	printf("Enter size of array : ");
+	scanf("%d",&a);
+	
+	int i,m[a];
+	
+	printf("\nEnter element of array :\n");
+	for(i=0; i<a; i++)
+	{
+		printf("a[%d] : ",i);
+		scanf("%d",&m[i]);
+	}
+	
+	int temp,j;
+	
+	for(i=0; i<a; i++)
+	{
+		for(j=i+1; j<a; j++)
+		{
+			if(m[i]<m[j])
+			{
+				temp=m[i];
+				m[i]=m[j];
+				m[j]=temp;
+			}
+		}
+	}
+	
+	printf("\nDecending Order : ");
+	
+	for(i=0; i<a; i++)
+	{
+		printf("%d ",m[i]);
+	}
+	
+}
