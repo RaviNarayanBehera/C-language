e1fe2d6cf414d2cb9ff16656c8b46714f7aa2106
Ravi Narayan Behera <148530184+RaviNarayanBehera@users.noreply.github.com>
2023-12-15 15:36:53 +0530

    Add files via upload

---
+++ String/Filament/2. Compare two string.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<string.h>
+main()
+{
+	char a[100],b[100];
+	
+	printf("Enter a string := ");
+	gets(a);
+	
+	printf("Enter a string := ");
+	gets(b);
+
+	
+	int i,check=0;
+	
+	for(i=0;a[i]!='\0';i++)
+	{
+		
+			if(a[i]==b[i])
+			{
+				check=1;
+			}
+			else
+			{
+				check=0;
+			}
+		
+	}
+	
+	if(check==1)
+		{
+			printf("String are same.");
+		}
+		else
+		{
+			printf("String are not same.");
+		}
+}
