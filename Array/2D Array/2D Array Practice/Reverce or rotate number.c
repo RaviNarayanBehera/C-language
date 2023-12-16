d10fd83c9616a736a35fa356cdb5a5830932bd6c
Ravi Narayan Behera <148530184+RaviNarayanBehera@users.noreply.github.com>
2023-12-16 23:02:43 +0530

    Add files via upload

---
+++ Array/2D Array/2D Array Practice/Reverce or rotate number.c
@@ -0,0 +1,57 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int m;
+	
+	printf("Enter size : ");
+	scanf("%d",&m);
+	
+	int i,j,a[m],temp=0;
+	
+	printf("\nEnter element : \n");
+	
+	for(i=0; i<m; i++)
+	{
+		printf("a[%d] : ",i);
+		scanf("%d",&a[i]);
+	}
+	for(i=0; i<m; i++)
+	{
+		for(j=i; j<m; j++)
+		{
+			if(a[i]>a[j])
+			{		
+				temp = a[i];
+				a[i] = a[j];
+				a[j] = temp;
+			}
+		}
+	}
+	
+	printf("\nLeft-rotate : ");
+	for(i=0; i<m; i++)
+	{
+		printf("%d ",a[i]);
+	}
+
+	for(i=0; i<m; i++)
+	{
+		for(j=i; j<m; j++)
+		{
+			if(a[i]<a[j])
+			{		
+				temp = a[i];
+				a[i] = a[j];
+				a[j] = temp;
+			}
+		}
+	}
+	
+	printf("\n\nRight-rotate : ");
+	for(i=0; i<m; i++)
+	{
+		printf("%d ",a[i]);
+	}
+	
+}
