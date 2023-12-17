6185a6f6f750e8347e9d10e6efb7282302111211
Ravi Narayan Behera <148530184+RaviNarayanBehera@users.noreply.github.com>
2023-12-17 19:38:06 +0530

    Add files via upload

---
+++ Looping/Pattern/Circlet/6.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int i,j,k;
+	
+	for(i=5; i>=1; i--)
+	{
+		for(k=i; k>1; k--)
+		{
+			printf(" ");
+		}
+		for(j=i; j<=5; j++)
+		{
+			printf("%d",j);
+		}
+		for(j=4; j>=i; j--)
+		{
+			printf("%d",j);
+		}
+		printf("\n");
+	}
+}
