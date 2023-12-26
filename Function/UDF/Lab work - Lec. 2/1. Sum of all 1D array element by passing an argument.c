0d63f3ec1bcb943c69fbe8b5969d956723c9f9b0
Ravi Narayan Behera <148530184+RaviNarayanBehera@users.noreply.github.com>
2023-12-26 19:13:00 +0530

    Add files via upload

---
+++ Function/UDF/Lab work - Lec. 2/1. Sum of all 1D array element by passing an argument.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<conio.h>
+void array(int sum)
+{
+	printf("\nSum of array is %d.",sum);
+}
+main()
+{
+	int m;
+	printf("Enter size of element : ");
+	scanf("%d",&m);
+	
+	int a[m];
+	printf("\nEnter elements of array : \n");
+	
+	int i,sum=0;
+	
+	for(i=0; i<m; i++)
+	{
+		printf("a[%d] = ",i);
+		scanf("%d",&a[i]);
+	}
+	
+	for(i=0; i<m; i++)
+	{
+		sum= sum + a[i];
+	}
+	
+	array(sum);
+}
