90131e1831f48c332bd53b74c567a08a967fc18f
Ravi Narayan Behera <148530184+RaviNarayanBehera@users.noreply.github.com>
2023-12-27 15:39:21 +0530

    Add files via upload

---
+++ Function/UDF/Recursion/2. Sum.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+int sum(int a)
+{
+	if(a==1)
+	{
+		return 1;
+	}
+	return a+sum(a-1);
+}
+void main()
+{
+	int a;
+	printf("Enter the value : ");
+	scanf("%d",&a);
+	
+	sum(a);
+	
+	printf("%d",sum(a));
+}
