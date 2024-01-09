8296b1ad74ed80fc07d805fbaae691c1098abe9d
Ravi Narayan Behera <148530184+RaviNarayanBehera@users.noreply.github.com>
2024-01-09 15:27:20 +0530

    Add files via upload

---
+++ Function/UDF/Menu Driven Calc.c
@@ -0,0 +1,69 @@
+#include<stdio.h>
+#include<conio.h>
+void calc()
+{
+	int a,b,op;
+	
+	start :
+		
+	printf("\nPress 1 for '+'\n");
+	printf("Press 2 for '-'\n");
+	printf("Press 3 for '*'\n");
+	printf("Press 4 for '/'\n");
+	printf("Press 5 for '%%'\n");
+	printf("Press 0 for the exit\n");
+	printf("\nEnter your choice : ");
+	scanf("%d",&op);
+	
+	if(op==1 || op==2 || op==3 || op==4 || op==5)
+	{
+		printf("Enter no.1 := ");
+		scanf("%d",&a);
+	
+		printf("Enter no.2 := ");
+		scanf("%d",&b);
+	}
+	
+	switch(op)
+	{
+		case 1 : 
+			printf("\nAnswer : Addition of %d and %d is %d.\n",a,b,a+b);
+			goto  start ;
+		break;
+		
+		case 2  : 
+			printf("\nAnswer : Substraction of %d and %d is %d.\n",a,b,a-b);
+			goto  start ;
+		break;
+		
+		case 3  : 
+			printf("\nAnswer : Multiplication of %d and %d is %d.\n",a,b,a*b);
+			goto  start ;
+		break;
+		
+		case 4 : 
+			printf("\nAnswer : Division of %d and %d is %d.\n",a,b,a/b);
+			goto  start ;
+		break;
+		
+		case 5 : 
+			printf("\nAnswer : Percentage of %d and %d is %d.\n",a,b,a%b);
+			goto  start ;
+		break;
+		
+		case 0 : 
+			printf("\nCode is Stop..");
+		break;
+		
+	
+		default :
+			printf("\nPlease enter valid Operator....!!\n");
+		break;		
+	}
+}
+
+
+main()
+{
+	calc();
+}
